4ea3a45164f64ecf345acb1f8f9b5c888d0fa0f4
Gbamele Alexandre <alexdelor2022@gmail.com>
2023-09-23 01:14:57 +0000

    map added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
 
-var nom = document.getElementById('nom').value;
-var prenom = document.getElementById('prenom').value;
-var telephone = document.getElementById('telephone').value;
-var email = document.getElementById('email').value;
+
 
 function afficherValeur() {
+
+    var prenom = document.getElementById('prenom').value;
+    var nom = document.getElementById('nom').value;
+    var prenom = document.getElementById('prenom').value;
+    var telephone = document.getElementById('telephone').value;
+    var email = document.getElementById('email').value;
+    
     alert(
         // "Nom : " + nom,
         "Prenom : " + prenom,
         // "Telephone : " + telephone,
         // "E-mail : " + email
     );
-    if(prenom.length <= 0){
+    if (prenom.length <= 0) {
         alert('champ invalide')
     }
-    else{
+    else {
         alert('champ valide')
     }
-    
+
 }
