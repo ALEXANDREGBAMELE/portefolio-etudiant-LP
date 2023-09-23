bd0b20aa1c7e16c3a0183c610d5f9c3797e5096a
Gbamele Alexandre <alexdelor2022@gmail.com>
2023-09-23 17:44:38 +0000

    map added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,11 @@
 
 function afficherValeur() {
 
-    var prenom = document.getElementById('prenom').value;
-    var nom = document.getElementById('nom').value;
-    var prenom = document.getElementById('prenom').value;
-    var telephone = document.getElementById('telephone').value;
-    var email = document.getElementById('email').value;
-    
+    const prenom = document.getElementById('prenom').value;
+    const nom = document.getElementById('nom').value;
+    const telephone = document.getElementById('telephone').value;
+    const email = document.getElementById('email').value;
+
     alert(
         // "Nom : " + nom,
         "Prenom : " + prenom,
@@ -23,3 +22,38 @@ function afficherValeur() {
     }
 
 }
+
+var boutton = document.getElementById('boutton');
+boutton.addEventListener('submit', function(event){
+    var nom = document.getElementById('nom');
+    event.preventDefault();
+    alert('nom')
+})
+
+// function afficherValeur() {
+// var formulaire = document.getElementById('formulaire');
+// formulaire.addEventListener('submit', function (event) {
+//     var nom = document.getElementById('nom');
+//     var prenom = document.getElementById('prenom');
+//     var messageNom = document.getElementById('messageNom');
+//     var messagePrenom = document.getElementById('messagePrenom');
+
+//     if (nom.value === '') {
+//         messageNom.innerHTML = 'Le champ Nom est obligatoire.';
+//         event.preventDefault();
+//     } else {
+//         messageNom.innerHTML = '';
+//     }
+
+//     if (prenom.value === '') {
+//         messagePrenom.innerHTML = 'Le champ Prénom est obligatoire.';
+//         event.preventDefault();
+//     } else {
+//         messagePrenom.innerHTML = '';
+//     }
+// });
+// }
+
+
+
+
